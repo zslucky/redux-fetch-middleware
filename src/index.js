6e4c1dfc95a63b6053e7749f6e2d57aaca236f20
Денис Шелест <axules@gmail.com>
2017-01-20 23:08:22 +0500

    Refactor + update
    FSA + response + react as response event

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import fetch from 'isomorphic-fetch';
 import merge from 'lodash.merge';
+import uuid from 'uuid';
 import defaultConfig from './config/defaultConfig';
 
-// Function return random UID
-function generateUid() {
-  function v4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return v4() + v4() + v4() + v4() + v4() + v4() + v4() + v4();
+function checkResponseType(type) {
+  const validType = String(type).toLowerCase();
+  const types = {
+    json: true,
+    text: true,
+    formData: true,
+    blob: true,
+    arrayBuffer: true,
+  };
+  return types[validType] ? validType : 'json';
 }
 
 function restMiddlewareCreator(customConfig) {
@@ -22,24 +25,38 @@ function restMiddlewareCreator(customConfig) {
     }
 
     const [REQUEST, SUCCESS, FAILURE] = suffix;
-    const { type, $payload, $props } = action;
-    const { url, options } = $payload;
+    const { type, $payload, meta } = action;
+    const { url, options, onResponse } = $payload;
     const opts = merge({}, fetchOptions, options);
-    // Generate UID request
-    const uid = generateUid();
+    // Check and get response type
+    const responseType = checkResponseType($payload.responseType);
+    // Generate UID request if meta.$uid is empty
+    const uid = meta && meta.$uid ? meta.$uid : uuid.v4();
+    const preMeta = merge({}, { $uid: uid }, meta);
+    let resultMeta = null;
     // Request start
-    dispatch({ type: `${type}_${REQUEST}`, $props, $uid: uid });
+    dispatch({ type: `${type}_${REQUEST}`, meta: preMeta });
 
     // Catch the response from service
     return fetch(url, opts)
-      .then(response => response.json())
+      .then((response) => {
+        resultMeta = merge({}, { $response: response }, preMeta);
+        if (
+          onResponse
+          && (onResponse instanceof Function)
+          && onResponse(response, preMeta, type) === false
+        ) {
+          return null;
+        }
+        return response[responseType]();
+      })
       .then((data) => {
         // Request success, dispatch the response data
-        dispatch({ type: `${type}_${SUCCESS}`, data, $props, $uid: uid });
+        dispatch({ type: `${type}_${SUCCESS}`, data, meta: resultMeta });
       })
       .catch((err) => {
         // Request failure, dispatch the error
-        dispatch({ type: `${type}_${FAILURE}`, err, $props, $uid: uid });
+        dispatch({ type: `${type}_${FAILURE}`, err, meta: resultMeta });
       });
   };
 }
